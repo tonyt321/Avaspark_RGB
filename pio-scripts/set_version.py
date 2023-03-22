9c9de10975283b92f16e2f56d355dadc5da4ff90
tonyt321 <gravesmarcello@gmail.com>
2023-03-22 02:41:07 +0000

    settings

--- pio-scripts/set_version.py
+++ pio-scripts/set_version.py
@@ -5,4 +5,4 @@ PACKAGE_FILE = "package.json"
 
 with open(PACKAGE_FILE, "r") as package:
     version = json.load(package)["version"]
-    env.Append(BUILD_FLAGS=[f"-DWLED_VERSION={version}"])
+ #   env.Append(BUILD_FLAGS=[f"-DWLED_VERSION={version}"])
--- wled00/my_config.h
+++ wled00/my_config.h
@@ -35,7 +35,7 @@
 //#define TEST_MODE //comment out for normal mode
 // disables turning lights off when detected from what should be the OW controller as off
 //#define PRO_VERSION    // comment out for standard
-#define SERVERNAME "ANDONN"
+//#define SERVERNAME "ANDONN"
 #define SHOP_NAME "Andonn"    // Andon Origin shop who installed it just andon for customer installed versions
 #define WLED_VERSION "2.2.2"  //shows up in info on UI
 
