2652fd52e30d9d342cd7f0311bdbda5d51b84b85
tonyt321 <gravesmarcello@gmail.com>
2024-03-16 02:03:47 +0000

    stock side flips

--- wled00/my_config.h
+++ wled00/my_config.h
@@ -25,8 +25,8 @@
   #define DEFAULT_LED_COUNT 13 //11 for GT 13 for pint
   #define OW_TYPE "Pint"
   #define WLED_VERSION "Pint 2.3.1"  //shows up in info on UI
-  #define FRONT_LIGHT_R_PIN 39   //front light red
-  #define FRONT_LIGHT_W_PIN 36   //front light white
+  #define FRONT_LIGHT_R_PIN 36   //front light red
+  #define FRONT_LIGHT_W_PIN 39   //front light white
 #endif
 
 #ifdef OW_GT
@@ -35,8 +35,8 @@
   #define DEFAULT_LED_COUNT 11 //11 for GT 13 for pint
   #define OW_TYPE "GT"
   #define WLED_VERSION "GT 2.3.1"  //shows up in info on UI
-  #define FRONT_LIGHT_R_PIN 39   //front light red
-  #define FRONT_LIGHT_W_PIN 36   //front light white
+  #define FRONT_LIGHT_R_PIN 36   //front light red
+  #define FRONT_LIGHT_W_PIN 39   //front light white
 #endif
 
 #ifdef OW_VESC
@@ -47,8 +47,8 @@
   #define WLED_VERSION "Vesc 2.3.1"  //shows up in info on UI
   #define VESC_RX 32   //vesc rx
   #define VESC_TX 33   //vesc tx
-  #define FRONT_LIGHT_R_PIN 39   //front light red
-  #define FRONT_LIGHT_W_PIN 36   //front light white
+  #define FRONT_LIGHT_R_PIN 36   //front light red
+  #define FRONT_LIGHT_W_PIN 39   //front light white
 #endif
 
 #define INTERFACE_UPDATE_COOLDOWN 2000 //time in ms to wait between websockets, alexa, and MQTT updates
